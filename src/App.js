02f443eb53d9058e1c8fbb861472e69b71f7f24c
kys <yskang@banaple.co.kr>
2023-01-26 13:38:06 +0900

    feat: mobx practice

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import PetList from "./components/PetList";
 import OwnerList from "./components/OwnerList";
 
 function App() {
-  const store = new PetOwnerStore();
-
+  const store = new PetOwnerStore(); // class 컴포넌트로 작성한 경우
   return (
     <div className="App">
       <h2>PetList</h2>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen position={`bottom-right`} />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
